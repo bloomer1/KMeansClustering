128bfcbdfbe9cff67cc36654de80be71f8c31cb1
Rahul Agarwal <rahulaga@usc.edu>
2015-09-25 21:28:50 -0700

    second commit

--- kMeans.py
+++ kMeans.py
@@ -1,6 +1,59 @@
 import sys
 
 
+def eucldn_distance(datapoint, cluster_center):
+	#print datapoint
+	#print cluster_center
+	p1 = datapoint.split(",")
+	p2 = cluster_center.split(",")
+	
+	print p1
+	print p2
+	
+	distance = 0
+	for cord in zip(p1,p2):
+		distance += (int(cord[0]) - int(cord[1]))*(int(cord[0]) - int(cord[1]))
+
+	print distance
+
+
+
+def kmeansClustering(data_records,k,init_method,threshold,maxIterations):
+
+	intial_centroids = list()
+	initial_clusters  = dict()
+	datapoints = list()
+
+	if init_method == 'first':
+		for datapoint_idx in data_records:
+			intial_centroids.append(data_records[datapoint_idx])
+			if len(intial_centroids) == k:
+				break
+		for idx in data_records:
+			datapoints.append(data_records[idx])
+		datapoints = datapoints[k:]
+
+	
+	elif init_method == 'rand':
+		print ''
+
+
+	#print intial_centroids
+	#print datapoints
+	for centroid in intial_centroids:
+		initial_clusters[centroid] = list()
+
+	#print initial_clusters
+	idx = 0;
+	for datapoint in datapoints:
+		d1 = eucldn_distance(datapoint,intial_centroids[idx])
+		#print d1
+		break
+	
+
+
+
+
 def process_dataset(dataset):
 
 	data_records = dict()
@@ -53,7 +106,8 @@ def main():
 	#print input_file
 
 	data_records = read_inputDataset(input_file)
-	
+
+	kmeansClustering(data_records,num_clusters,init_method,con_thrlsd,max_itr)
 
 
 
